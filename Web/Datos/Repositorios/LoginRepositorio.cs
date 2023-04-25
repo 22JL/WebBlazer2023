34f05dbecfc2627e786ffef38064108f8bf63a72
22JL <jacky.lara911@gmail.com>
2023-04-24 20:43:28 -0600

    actualizacion

---
+++ Web/Datos/Repositorios/LoginRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Datos.Interfaces;
+using Entidades;
+using MySql.Data.MySqlClient;
+
+namespace Datos.Repositorios
+{
+    public class LoginRepositorio : ILoginRepositorio
+    {
+        private string CadenaConexion;
+        public LoginRepositorio(string _cadenaConexion)
+        {
+            CadenaConexion = _cadenaConexion;
+        }
+        private MySqlConnection Conexion()
+        {
+            return new MySqlConnection(CadenaConexion);
+        }
+        public async Task<bool> ValidarUsuarioAsync(Login Login)
+        {
+            bool valido = false;
+            try
+            {
+                using MySqlConnection _conexion = Conexion();
+                await _conexion.OpenAsync();
+                string sql = "SELECT 1 FROM usuario WHERE CodigoUsuario = @CodigoUsuario And Contrasena = @Contrasena;";
+                valido = await _conexion.ExecuteScalarAsync<bool>(sql, new { Login.CodigoUsuario, Login.Contraseña });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return valido;
+        }
+    }
+}
---
+++ Web/Datos/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Datos.Interfaces;
+using Modelos;
+using MySql.Data.MySqlClient;
+
+namespace Datos.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private string CadenaConexion;
+        public UsuarioRepositorio(string _cadenaConexion)
+        {
+            CadenaConexion = _cadenaConexion;
+        }
+        private MySqlConnection Conexion()
+        {
+            return new MySqlConnection(CadenaConexion);
+        }
+
+
+        public async Task<bool> ActualizarAsync(Usuario usuario)
+        {
+            bool resultado = false;
+            try
+            {
+                using MySqlConnection _conexion = Conexion();
+                await _conexion.OpenAsync();
+                string sql = @"UPDATE Usuarios SET Nombre = @Nombre,  Contrasena = @Contrasena, Correo = @Correo, Rol =@Rol, Foto = @Foto, EstadoActivo = @EstadoActivo 
+                               WHERE CodigoUsuario = @CodigoUsuario;";
+                resultado = Convert.ToBoolean(await _conexion.ExecuteAsync(sql, usuario));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return resultado;
+        }
+
+        public async Task<bool> EliminarAsync(string codigo)
+        {
+            bool resultado = false;
+            try
+            {
+                using MySqlConnection _conexion = Conexion();
+                await _conexion.OpenAsync();
+                string sql = @" DELETE FROM Usuarios WHERE CodigoUsuario = @CodigoUsuario;";
+                resultado = Convert.ToBoolean(await _conexion.ExecuteAsync(sql, new { codigo }));
+            }
+            catch (Exception)
+            {
+            }
+            return resultado;
+        }
+
+        public Task<IEnumerable<Usuario>> GetListaAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> GetPorCodigoAsync(string codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> NuevoAsync(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
